388e1ee3423848a4ad81d5c198850f1f74ff73e7
zaidbbhjs <151960814+zaidbbhjs@users.noreply.github.com>
2024-03-30 20:39:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import requests,time
 while True:
     re=requests.get('https://fb762d69-1c64-407d-ba30-b6a5344f068a-00-ar4py2fveshx.sisko.replit.dev/api/insta/fhdhasd3232@gmail.com').text
     print(re)
-    time.sleep(1.5)
+    time.sleep(5.5)
