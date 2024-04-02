72824c019ca27c56f86a55f4643cc77c98dad210
zaidbbhjs <151960814+zaidbbhjs@users.noreply.github.com>
2024-04-02 15:33:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -123,4 +123,4 @@ async def ga(gmail):
     elif "Bad Email" in df:
     	return "Bad"
     else:return "Error"
-uvicorn.run(app,host='0.0.0.0',port=8080)
+#uvicorn.run(app,host='0.0.0.0',port=8080)
